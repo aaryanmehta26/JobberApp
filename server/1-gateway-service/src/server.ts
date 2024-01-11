c8f1b2b4c51048bb196a3ebf703a90133319af2e
Aaryan Mehta <aaryanmehta@Aaryans-MacBook-Pro.local>
2024-01-11 23:57:13 +0530

    Api gateway in progress

---
+++ server/1-gateway-service/src/server.ts
@@ -0,0 +1,110 @@
+import * as http from 'http';
+
+import { Logger } from 'winston';
+import {Application, NextFunction, Request, Response, json, urlencoded} from 'express';
+import { StatusCodes } from 'http-status-codes';
+import cookieSession from 'cookie-session';
+import hpp from 'hpp';
+import helmet from 'helmet';
+import cors from 'cors';
+import compression from 'compression';
+import { isAxiosError } from 'axios';
+
+import { CustomError, IErrorResponse, winstonLogger } from '../../jobber-shared/src';
+
+
+const SERVER_PORT = 4000;
+const DEFAULT_ERROR_CODE = 500;
+const log: Logger = winstonLogger('http://localhost:9200', 'apiGatewayServer', 'debug');
+
+export class GatewayServer {
+  private app: Application;
+
+  constructor(app: Application) {
+    this.app = app;
+  }
+
+  public start(): void {
+    this.securityMiddleware(this.app);
+    this.standardMiddleware(this.app);
+    this.routesMiddleware();
+    this.startElasticSearch();
+    this.errorHandler(this.app);
+    this.startServer(this.app);
+  }
+
+  private securityMiddleware = (app: Application): void => {
+    app.set('trust-proxy', 1);
+    app.use(
+      cookieSession({
+        name: 'session',
+        keys: [],
+        maxAge: 24 * 7 * 3600000,
+        secure: false
+      })
+    );
+    app.use(hpp());
+    app.use(helmet());
+    app.use(cors({
+      origin: '',
+      credentials: true,
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
+    }));
+  };
+
+  private standardMiddleware = (app: Application): void => {
+    app.use(compression);
+    app.use(json({limit: '200mb'}));
+    app.use(urlencoded({extended: true, limit: '200mb'}));
+  };
+
+  private routesMiddleware = (): void => {
+
+  };
+
+  private startElasticSearch(): void {
+  }
+
+  private errorHandler(app: Application): void {
+    app.use('*', (req: Request, res: Response, next: NextFunction) => {
+      const fullUrl = `${req.protocol}://${req.get('host')}${req.originalUrl}`;
+      log.log('error', `${fullUrl} endpoint does not exist.`, '');
+      res.status(StatusCodes.NOT_FOUND).json({ message: 'The endpoint called does not exist.'});
+      next();
+    });
+
+    app.use((error: IErrorResponse, _req: Request, res: Response, next: NextFunction) => {
+      if (error instanceof CustomError) {
+        log.log('error', `GatewayService ${error.comingFrom}:`, error);
+        res.status(error.statusCode).json(error.serializeErrors());
+      }
+
+      if (isAxiosError(error)) {
+        log.log('error', `GatewayService Axios Error - ${error?.response?.data?.comingFrom}:`, error);
+        res.status(error?.response?.data?.statusCode ?? DEFAULT_ERROR_CODE).json({ message: error?.response?.data?.message ?? 'Error occurred.' });
+      }
+
+      next();
+    });
+  }
+
+  private async startServer(app: Application): Promise<void> {
+    try {
+      const httpServer: http.Server = new http.Server(app);
+      this.startHttpServer(httpServer);
+    } catch (error) {
+      log.log('error', 'GatewayService startServer() error method:', error);
+    }
+  }
+
+  private async startHttpServer(httpServer: http.Server): Promise<void> {
+    try {
+      log.info(`Gateway server has started with process id ${process.pid}`);
+      httpServer.listen(SERVER_PORT, () => {
+        log.info(`Gateway server running on port ${SERVER_PORT}`);
+      });
+    } catch (error) {
+      log.log('error', 'GatewayService startServer() error method:', error);
+    }
+  }
+}
